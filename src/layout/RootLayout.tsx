578b1ad3447058f3c15bf14029cc051ff1ca53c1
Kavin Daniel <kavindaniel99@gmail.com>
2025-06-29 23:13:00 +0530

    blog

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../Componants/Header"
 import Footer from "../Componants/Footer"
-import React from "react"
 
 
 const RootLayout = () => {
